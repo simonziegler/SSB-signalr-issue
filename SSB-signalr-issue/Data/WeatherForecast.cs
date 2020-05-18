fc28ec8f46d58e10a794b15bca952aa543405b79
Simon Ziegler <simon@dioptra.tech>
2020-05-18 09:06:31 +0100

    Add project files.

---
+++ SSB-signalr-issue/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SSB_signalr_issue.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
